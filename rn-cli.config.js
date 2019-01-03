ec722798dbd78a8900d32bf6d4d3549aaefbae7c
marcosmartinez7 <martinez.marcos@live.com>
2019-01-03 14:16:08 -0300

    Initial commit

---
+++ rn-cli.config.js
@@ -0,0 +1,19 @@
+const extraNodeModules = require('node-libs-browser');
+
+
+module.exports = {
+    resolver: {
+        extraNodeModules,
+    },
+    transformer: {
+        /* transformer options */
+    },
+    serializer: {
+        /* serializer options */
+    },
+    server: {
+        /* server options */
+    }
+
+    /* general options */
+};
